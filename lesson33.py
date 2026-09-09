61fffd597641cf79c3d9277eadb4ff0299697c76
kotopes <dima.08ermakov@gmail.com>
2026-09-09 19:56:24 +0300

    Add files via upload

---
+++ lesson33.py
@@ -0,0 +1,15 @@
+print("Практическое занятие №3")
+print("Я изучаю Git")
+print("Git помогает сохранять историю проекта")
+def приветствие(Дмитрий):
+    print(f"Привет,{Дмитрий}! Работаем с Git.")
+приветствие("Студент")
+
+name="Дмитрий"
+group="1ИИ-2-11-26"
+skills=["Python", "NumPy", "Pandas", "Git"]
+
+print("Имя:", name)
+print("Группа:", group)
+for skill in skills:
+    print("-",skill)
